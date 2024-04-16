13d934b801dcf00c20ef91bd23257a00dd98ad7b
LUV SINGH <luv13singh@gmail.com>
2024-04-16 14:08:36 +0530

    update main.py

--- main.py
+++ main.py
@@ -41,22 +41,27 @@ dotenv.load_dotenv("ops/.env")
 
 token = os.getenv('TELEGRAM_BOT_TOKEN')
 
+# Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-class BotInitializer:
-    _instance = None
-    run_once = False
+# setting higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(BotInitializer, cls).__new__(cls)
-            cls.run_once = True
-        return cls._instance
+logger = logging.getLogger(__name__)
+
+# class BotInitializer:
+#     _instance = None
+#     run_once = False
+
+#     def __new__(cls):
+#         if cls._instance is None:
+#             cls._instance = super(BotInitializer, cls).__new__(cls)
+#             cls.run_once = True
+#         return cls._instance
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    BotInitializer()  # To initialize only once
+    # BotInitializer()  # To initialize only once
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -105,12 +110,12 @@ async def preferred_language_callback(update: Update, context: CallbackContext):
     elif lang == "mr":
         text_message = "तुम्ही मराठीची निवड केली आहे. \कृपया मला तुमची समस्या सांगा"
 
-    set_redis('lang', lang)
-
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=text_message
     )
+    
+    # set_redis('lang', lang)
 
 
 async def response_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -168,6 +173,75 @@ if __name__ == '__main__':
     application.add_handler(response_handler)
     application.run_polling()
     
+
+
+'''
+"""
+Basic example for a bot that uses inline keyboards. For an in-depth explanation, check out
+ https://github.com/python-telegram-bot/python-telegram-bot/wiki/InlineKeyboard-Example.
+"""
+import logging
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with three inline buttons attached."""
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1"),
+            InlineKeyboardButton("Option 2", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    await query.answer()
+
+    await query.edit_message_text(text=f"Selected option: {query.data}")
+
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Displays info on how to use the bot."""
+    await update.message.reply_text("Use /start to test this bot.")
+
+
+def main() -> None:
+    """Run the bot."""
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token("TOKEN").build()
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+    main()
+'''
 
 # async def chat_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, text: str):
 #     response = ""
